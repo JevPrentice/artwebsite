c1cc040cee69a0658ff3f10715c89f65c4b25f35
simonec505 <scoetzer50@gmail.com>
2016-04-23 17:09:44 +0200

    text animation on Commissions and Process page

--- public/js/grayscale.js
+++ public/js/grayscale.js
@@ -42,26 +42,48 @@
 //     }    
 // });
 
-var firstScroll = false;
 
-$(document).scroll(function(){
-    if($(this).scrollTop()>=$('#commissions_and_process_scroll_destination').position().top && !firstScroll){
-        textOutImagesIn();
+////////////////////////// FOR COMMISSIONS AND PROCESS PAGE ANIMATION
+$(document).ready(function(){
+    var firstScroll = false;
+    $("#CommissionsCarousel").css('opacity', '0');
+    //$(".CommissionProcess").hide();
+
+
+    $(document).scroll(function(){
+        if($(this).scrollTop()>=$('#commissions_and_process_scroll_destination').position().top && !firstScroll){
+            textOutImagesIn();
+        }
+        
+
+    })
+
+
+    function textOutImagesIn(){
+
+            //$(".poem").show();
+            $(".poemAnimate").textillate({
+                in: {
+                    effect: 'fadeIn'
+                }
+            });
+
+            setTimeout(function() { $(".poem").fadeOut(3000); }, 13000);
+
+            setTimeout(function(){opacityAnimation()}, 13000);
+            $('.carousel').carousel(3); 
+
+            firstScroll=true;
+            
     }
-    else{$(".CommissionProcess").show();}
 
-})
+    function opacityAnimation(){
+        $("#CommissionsCarousel").animate({'opacity': '1'}, 3000);
+    }
+ 
+});
 
-
-function textOutImagesIn(){
-        $("#poem").show();
-        setTimeout(function() { $("#poem").fadeOut(2000); }, 5000);
-
-        $(".CommissionProcess").hide();
-        setTimeout(function(){ $(".CommissionProcess").fadeIn(2000);}, 7000);
-        $('.carousel').carousel(4); 
-        firstScroll=true;
-}
+//////////////////////////////
 
 $(function() {
     $('.modal').on("show.bs.modal", function(){
@@ -80,12 +102,13 @@ $(document).ready(function(){
 
   $('.slick_about').slick({
     autoplay: true,
-    autoplaySpeed: 7000,
+    autoplaySpeed: 5000,
     fade: true,
     speed: 4000,
     accessibility: false,
     arrows: false,
     focusOnSelect: true,
+    mobileFirst: true,
   });
 });
 
@@ -129,164 +152,6 @@ $('.navbar-collapse ul li a').click(function() {
   }
 });
 
-// Google Maps Scripts
-// var map = null;
-// // When the window has finished loading create our google map below
-// google.maps.event.addDomListener(window, 'load', init);
-// google.maps.event.addDomListener(window, 'resize', function() {
-//     map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
-// });
-
-// function init() {
-//     // Basic options for a simple Google Map
-//     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-//     var mapOptions = {
-//         // How zoomed in you want the map to start at (always required)
-//         zoom: 15,
-
-//         // The latitude and longitude to center the map (always required)
-//         center: new google.maps.LatLng(40.6700, -73.9400), // New York
-
-//         // Disables the default Google Maps UI components
-//         disableDefaultUI: true,
-//         scrollwheel: false,
-//         draggable: false,
-
-//         // How you would like to style the map. 
-//         // This is where you would paste any style found on Snazzy Maps.
-//         styles: [{
-//             "featureType": "water",
-//             "elementType": "geometry",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 17
-//             }]
-//         }, {
-//             "featureType": "landscape",
-//             "elementType": "geometry",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 20
-//             }]
-//         }, {
-//             "featureType": "road.highway",
-//             "elementType": "geometry.fill",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 17
-//             }]
-//         }, {
-//             "featureType": "road.highway",
-//             "elementType": "geometry.stroke",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 29
-//             }, {
-//                 "weight": 0.2
-//             }]
-//         }, {
-//             "featureType": "road.arterial",
-//             "elementType": "geometry",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 18
-//             }]
-//         }, {
-//             "featureType": "road.local",
-//             "elementType": "geometry",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 16
-//             }]
-//         }, {
-//             "featureType": "poi",
-//             "elementType": "geometry",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 21
-//             }]
-//         }, {
-//             "elementType": "labels.text.stroke",
-//             "stylers": [{
-//                 "visibility": "on"
-//             }, {
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 16
-//             }]
-//         }, {
-//             "elementType": "labels.text.fill",
-//             "stylers": [{
-//                 "saturation": 36
-//             }, {
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 40
-//             }]
-//         }, {
-//             "elementType": "labels.icon",
-//             "stylers": [{
-//                 "visibility": "off"
-//             }]
-//         }, {
-//             "featureType": "transit",
-//             "elementType": "geometry",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 19
-//             }]
-//         }, {
-//             "featureType": "administrative",
-//             "elementType": "geometry.fill",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 20
-//             }]
-//         }, {
-//             "featureType": "administrative",
-//             "elementType": "geometry.stroke",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 17
-//             }, {
-//                 "weight": 1.2
-//             }]
-//         }]
-//     };
-
-//     // Get the HTML DOM element that will contain your map 
-//     // We are using a div with id="map" seen below in the <body>
-//     var mapElement = document.getElementById('map');
-
-//     // Create the Google Map using out element and options defined above
-//     map = new google.maps.Map(mapElement, mapOptions);
-
-//     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-//     var image = 'img/map-marker.png';
-//     var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-//     var beachMarker = new google.maps.Marker({
-//         position: myLatLng,
-//         map: map,
-//         icon: image
-//     });
-// }
-
-/*** my code ***/
-
-// $(document).ready(funtion(){
-//     $('.beginning-text.hidden').fadeIn(1000).removeClass('.beginning-text.hidden');
-// })
-
 
 
 $('.info').click(function() {
@@ -311,12 +176,12 @@ $('.see_less').click(function(){
 
 //works -- but makes website slow...
             $('.carousel').carousel({
-              interval: 7000
+              interval: 3500
             })
 
  $(window).bind("load", function(){
         $(document).ready(function(){
-            $('#bg').fadeIn(2000);
+            $('#bg').fadeIn(4000);
             $('#fade_in_words').fadeIn(4000);
             //$('#fade_in_words').show("scale", {}, 2000) 
         })
