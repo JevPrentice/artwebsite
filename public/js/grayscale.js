4e8f89ab16feabd69c9e608d269ef20b8680e970
simonec505 <scoetzer50@gmail.com>
2016-06-19 20:42:38 +0200

    lazy load carousels

--- public/js/grayscale.js
+++ public/js/grayscale.js
@@ -127,7 +127,7 @@ $(function() {
 
 $("div.lazy").lazyload({
       effect : "fadeIn",
-      threshold: 500
+      //threshold: -500
   });
 
 //////////////////////////////
@@ -250,7 +250,39 @@ $('.carousel').carousel({
             })
 
 
+/////////////////////////////////////
 
 
+var cHeight = 0;
+
+$('#CommissionsCarousel, #SelfPortraitsCarousel').on('slide.bs.carousel', function (e) {
+    var $nextImage = null;
+
+    $activeItem = $('.active.item', this);
+
+    if (e.direction == 'left'){
+        $nextImage = $activeItem.next('.item').find('img');
+    } else {
+        if ($activeItem.index() == 0){
+            $nextImage = $('img:last', $activeItem.parent());
+        } else {
+            $nextImage = $activeItem.prev('.item').find('img');
+        }
+    }
+
+    // prevents the slide decrease in height
+    if (cHeight == 0) {
+       cHeight = $(this).height();
+       $activeItem.next('.item').height(cHeight);
+    }
+
+    // prevents the loaded image if it is already loaded
+    var src = $nextImage.data('lazy-load-src');
+
+    if (typeof src !== "undefined" && src != "") {
+       $nextImage.attr('src', src)
+       $nextImage.data('lazy-load-src', '');
+    }
+});
 
 
