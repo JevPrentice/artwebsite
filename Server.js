751f12652dff0470447eb01aa9661cd4b310c13f
JevPrentice <jevprentice@gmail.com>
2016-11-27 13:14:24 +0200

    Minor refactoring

--- Server.js
+++ Server.js
@@ -1,12 +1,15 @@
+/* global sendMessage */
+
 var fs = require('fs');
 var express = require("express");
 var app = express();
 var router = express.Router();
-
 var bodyParser = require('body-parser');
+require('util');
 
-eval(fs.readFileSync('googleapis_integration.js', "utf-8").toString());
+eval(fs.readFileSync('googleapis_integration.js', "utf-8").toString()); //TODO do this properly
 
+// Configure app to use bodyParser(), this will get the data from a POST
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
@@ -21,52 +24,61 @@ router.get("/", function (req, res) {
     res.sendFile("public/index.html");
 });
 
+function isProbablySpam(message) {
+    return message.includes("It is free to sign up")
+            || message.includes("and they let you try their service for free for 7 days.")
+}
+
+function getEmailLog(name, email, phone, message) {
+    var datetime = new Date(new Date().getTime()).toISOString();
+    return util.format('{"datetime":"%s","name":"%s","email":"%s","phone":"%s","message":"%s"}',
+            datetime, name, email, phone, message);
+}
+
+function getEmailText(name, email, phone, message) {
+    return 'Name: ' + name + "\nEmail: " + email + "\nPhone:" + phone + "\nMessage:" + message;
+}
+
+
 app.post('/contactForm', function (req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var phone = req.body.phone;
+    var message = req.body.message;
 
-    var dateLocaleString = new Date().toLocaleString();
-    var logString = "Contact Form POST: dateLocaleString='" + dateLocaleString + "',name='" + req.body.name + "',email='" + req.body.email + "',phone='" + req.body.phone + "',message='" + req.body.message + "'";
-    console.log("******************************\nPOST Request Recieved at /contactForm req=" + req + " res=" + res + "logString=" + logString);
+    var logString = getEmailLog(name, email, phone, message);
+    console.log(logString);
 
-    try {
-        fs.readFile('email_log.json', 'utf8', function (err, data) {
-            if (err) {
-                console.log("fs is unable to readFile email_log.json. err=" + err + " data=" + data);
-            } else {
-                data += "\n" + logString;
-                fs.writeFile("email_log.json", data, "utf8", null);
-            }
-        });
-    } catch (err) {
-        console.log("Error while logging to email_log.son . err=" + err + " logString=" + logString);
-    }
+    fs.readFile('email.log', 'utf8', function (err, data) {
+        if (err) {
+            throw err;
+        }
 
-    try {
-        fs.readFile('client_secret.json', function (err, data) {
-            if (err) {
-                console.log("fs is unable to readFile client_secret.json. err=" + err + " data=" + data);
-            } else {
+        if (data.length === 0) {
+            data = "[" + logString + "]";
+        } else {
+            var position = data.lastIndexOf("]");
+            var newValue = data.substr(0, position) + ",\n" + logString + data.substr(position);
+            data = newValue;
+        }
+        fs.writeFile("email_log.json", data, "utf8", null);
+    });
 
-                try {
-                    authorize(JSON.parse(data), logString, sendMessage);
-                } catch (err) {
-                    console.log("Error while performing googleapis integration. err=" + err + " logString=" + logString);
-                }
-            }
-        });
-    } catch (err) {
-        console.log("Error while logging fs . err=" + err + " logString=" + logString);
-    }
+    // Load client secrets from a local file.
+    fs.readFile('client_secret.json', function (err, content) {
+        if (err) {
+            console.log('Error loading client secret file: ' + err);
+            return;
+        }
+        // Authorize a client with the loaded credentials, then call the Gmail API.
+        var messageText = getEmailText(name, email, phone, message);
+        authorize(JSON.parse(content), messageText, sendMessage);
+    });
 
-    /*function authorizeCallback(oauth2Client, messageText) {
-     console.log("Authorize Callback - " + messageText);
-     }*/
-
+    /*function authorizeCallback(oauth2Client, messageText) {console.log("Authorize Callback - " + messageText);}*/
     res.sendStatus(200);
-
-    console.log("******************************\nResponding to /contactForm with 200 req=" + req + " res=" + res + "logString=" + logString);
-
 });
 
 app.listen(80, function () {
-    console.log("Live at Port 80");
+    console.log("Exploring the unseen on Port 80");
 });
